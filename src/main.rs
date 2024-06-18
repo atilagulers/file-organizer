03cedfb019ff192cf81615852a6388e3c7c416a3
Atila Guler <atilagulerdev@gmail.com>
2024-06-18 23:28:02 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-use std::fs::{self, DirEntry};
+use std::error::Error;
+use std::fs::{self, DirEntry, ReadDir};
+use std::io;
 use std::process;
 
 fn main() {
     // get a folder
-    let entries = fs::read_dir("test_dir").unwrap_or_else(|err| {
-        println!("Failed to read directory, Error: {}", err);
-        process::exit(1);
-    });
 
     let mut files: Vec<DirEntry> = Vec::new();
 
-    for entry in entries {
-        let entry = entry.unwrap();
-        println!("{:?}", entry);
-        files.push(entry);
-    }
+    //for entry in entries {
+    //    let entry = entry.unwrap();
+    //    let metadata = entry.metadata().unwrap();
+    //    let is_file = metadata.is_file();
+    //    println!("{:#?} {}", entry, is_file);
+    //    //files.push(entry);
+    //}
 
-    println!("{:#?}", files[1].file_type());
+    //println!("{:#?}", files[1]);
     // list filenames inside of the folder
+
+    if let Err(e) = run() {
+        println!("Application error: {}", e);
+        process::exit(1)
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let entries = fs::read_dir(path)?;
+
+    Ok(())
 }
 
 //struct Directory {
