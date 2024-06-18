1f6a8fd33664c3028dc85e7720985580b7c70436
Atila Guler <atilagulerdev@gmail.com>
2024-06-19 00:21:40 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
 use std::error::Error;
+use std::ffi::OsStr;
 use std::fs::{self, DirEntry, ReadDir};
 use std::io;
+use std::path::Path;
 use std::process;
 
 fn main() {
     // get a folder
-
-    let mut files: Vec<DirEntry> = Vec::new();
-
-    //for entry in entries {
-    //    let entry = entry.unwrap();
-    //    let metadata = entry.metadata().unwrap();
-    //    let is_file = metadata.is_file();
-    //    println!("{:#?} {}", entry, is_file);
-    //    //files.push(entry);
-    //}
-
-    //println!("{:#?}", files[1]);
-    // list filenames inside of the folder
 
     if let Err(e) = run() {
         println!("Application error: {}", e);
@@ -26,30 +15,40 @@ fn main() {
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
-    let entries: ReadDir = fs::read_dir("test_dir")?;
-    let mut files: Vec<DirEntry> = Vec::new();
+    let mut directory = Directory::new("test_dir");
 
-    // extract files
-    for entry in entries {
-        let entry = entry?;
+    directory.files = get_files("test_dir")?;
 
-        if entry.path().is_file() == true {
-            println!("{:#?}", entry);
-            files.push(entry);
-        }
-    }
-
+    println!("{:#?}", directory.files);
     Ok(())
 }
 
-//struct Directory {
-//    name: String,
-//}
+fn get_files(dir: &str) -> Result<Vec<DirEntry>, io::Error> {
+    let entries: ReadDir = fs::read_dir(dir)?;
+    let mut files: Vec<DirEntry> = Vec::new();
 
-//impl Directory {
-//    fn new(name: &str) -> Self {
-//        Folder {
-//            name: name.to_string(),
-//        }
-//    }
-//}
+    let path = Path::new(dir);
+
+    // extract files
+    for entry in entries {
+        if let Ok(entry) = entry {
+            println!("{:?}", entry.path());
+        }
+    }
+
+    Ok(files)
+}
+
+struct Directory {
+    name: String,
+    files: Vec<DirEntry>,
+}
+
+impl Directory {
+    fn new(name: &str) -> Self {
+        Directory {
+            name: name.to_string(),
+            files: Vec::new(),
+        }
+    }
+}
