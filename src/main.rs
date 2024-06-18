fce50233f18a436aaa322c2a6ebbae4cddb58ccf
Atila Guler <atilagulerdev@gmail.com>
2024-06-18 21:50:00 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-use std::{fs, process};
+use std::fs::{self, DirEntry};
+use std::process;
 
 fn main() {
-    println!("Hello, world!");
-
     // get a folder
-    let folder = fs::read_dir("test_dir").unwrap_or_else(|err| {
+    let entries = fs::read_dir("test_dir").unwrap_or_else(|err| {
         println!("Failed to read directory, Error: {}", err);
         process::exit(1);
     });
 
-    for entry in folder {
-        println!("Entry: {:?}", entry);
+    let mut files: Vec<DirEntry> = Vec::new();
+
+    for entry in entries {
+        let entry = entry.unwrap();
+        println!("{:?}", entry);
+        files.push(entry);
     }
 
+    println!("{:#?}", files[1].file_type());
     // list filenames inside of the folder
 }
+
+//struct Directory {
+//    name: String,
+//}
+
+//impl Directory {
+//    fn new(name: &str) -> Self {
+//        Folder {
+//            name: name.to_string(),
+//        }
+//    }
+//}
