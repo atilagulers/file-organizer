14531dee70f2b1e1c0c80969bde4c4e92b1101ad
Atila Guler <atilagulerdev@gmail.com>
2024-06-19 18:32:31 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,31 @@ use std::process;
 fn main() {
     // get a folder
 
-    if let Err(e) = run() {
-        eprintln!("Application error: {}", e);
-        process::exit(1)
+    eprintln!("Please provide a folder path");
+
+    let target_path = io::stdin()
+        .lines()
+        .next()
+        .expect("No input found")
+        .expect("Failed to read line");
+
+    let pb = indicatif::ProgressBar::new(100);
+    for i in 0..100 {
+        if let Err(e) = run(&target_path) {
+            eprintln!("Application error: {}", e);
+            process::exit(1)
+        }
+
+        if i % 5 == 0 {
+            pb.println(format!("[+] finished #{}", i));
+        }
+        pb.inc(5);
     }
+    pb.finish_with_message("done");
 }
 
-fn run() -> Result<(), Box<dyn Error>> {
-    const DIRECTORY_PATH: &str = "test_dir";
-
-    let dir_path: PathBuf = PathBuf::from(DIRECTORY_PATH);
+fn run(target_path: &str) -> Result<(), Box<dyn Error>> {
+    let dir_path: PathBuf = PathBuf::from(target_path);
     let mut files = walk_dir(&dir_path)?;
 
     let mut extensions: HashMap<String, u32> = HashMap::new();
@@ -56,7 +71,6 @@ fn run() -> Result<(), Box<dyn Error>> {
         let new_file_path = ext_dir.join(&file.file_name);
         fs::copy(&file.path, &new_file_path)?;
     }
-    //println!("{:#?}", new_folder);
 
     Ok(())
 }
@@ -80,7 +94,6 @@ fn walk_dir(path: &PathBuf) -> Result<Vec<File>, Box<dyn Error>> {
                     .unwrap_or("no_extension");
                 let file = File::new(path.to_str().unwrap(), file_name, extension);
                 files.push(file);
-                //println!("{:#?} is pushed to files", file);
             }
         }
     }
